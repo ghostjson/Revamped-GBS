dcfe7790a5628a047e385edadcf244ceee55f05f
Akhil Ravindran <imakhilravindran@gmail.com>
2020-05-21 14:50:09 +0530

    initialize project
    create index page
    create about page
    create contact page

---
+++ assets/js/index.js
@@ -0,0 +1,14 @@
+$('#main-banner-carousel').carousel({
+})
+
+$('.carousel-control-next').click(function(){
+    $('#main-banner-carousel').carousel('next')
+})
+
+$('.carousel-control-prev').click(function(){
+    $('#main-banner-carousel').carousel('prev')
+})
+
+$(document).ready(function(){
+    $('#main-banner-carousel').carousel(0)
+})
